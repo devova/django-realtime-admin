3193837eb8e7a57b8675d31909eb0d73f0be1322
Volodymyr Trotsyshyn <volodymyr.trotsyshyn@tictrac.com>
2019-08-24 22:26:11 +0300

    Drone

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description='',
     author='Volodymyr Trotsyshyn',
     author_email='devova@gmail.com',
-    url='https://github.com/devova/pytest-yield',
+    url='https://github.com/devova/django-realtime-admin',
     py_modules=['django_realtime_admin'],
     install_requires=["django>=1.11,<2.0", "django_eventstream"],
     keywords=['testing', 'pytest'],
